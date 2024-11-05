7467cdfa553470e6aef4661a86137f967753e572
Taras Ostapchuk <trs.ostp@gmail.com>
2024-11-05 18:34:07 +0100

    Review some code bags - Implement Token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -90,14 +90,15 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('token', res.data)
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-            """Test posting a blank password returns an error."""
+        """Test posting a blank password returns an error."""
             
-            payload = {'email': 'test@example.com','password': ''}
-            res = self.client.post(TOKEN_URL, payload)
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
 
-            self.assertIn('token', res.data)
-            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
